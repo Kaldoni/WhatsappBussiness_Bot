f11388ba0df61cd11d88546d8a25e4737e2e31fd
Kaldoni <kaliodaniel7412@gmail.com>
2025-11-28 01:20:06 +0100

    updated

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Bot running successfully");
+});
+
+
 app.post("/chat", async (req, res) => {
     const { message } = req.body;
 
